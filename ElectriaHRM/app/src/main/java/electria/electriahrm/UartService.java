1a5d6ce1c62aa9b13bb04e71a0c1d88145ceb15e
Augustine Onubeze <augustine.onubeze@metropolia.fi>
2015-04-14 12:00:23 +0300

    Modify connect UartService function to directly connect to device as well as reconnect initial connection if it fails

--- ElectriaHRM/app/src/main/java/electria/electriahrm/UartService.java
+++ ElectriaHRM/app/src/main/java/electria/electriahrm/UartService.java
@@ -224,10 +224,11 @@ public class UartService extends Service {
      * Connects to the GATT server hosted on the Bluetooth LE device.
      *
      * @param address The device address of the destination device.
+     *
      * @return Return true if the connection is initiated successfully. The connection result
-     * is reported asynchronously through the
-     * {@code BluetoothGattCallback#onConnectionStateChange(android.bluetooth.BluetoothGatt, int, int)}
-     * callback.
+     *         is reported asynchronously through the
+     *         {@code BluetoothGattCallback#onConnectionStateChange(android.bluetooth.BluetoothGatt, int, int)}
+     *         callback.
      */
     public boolean connect(final String address) {
         if (mBluetoothAdapter == null || address == null) {
@@ -246,3 +247,17 @@ public class UartService extends Service {
                 return false;
             }
         }
+
+        final BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+        if (device == null) {
+            Log.w(TAG, "Device not found.  Unable to connect.");
+            return false;
+        }
+        // We want to directly connect to the device, so we are setting the autoConnect
+        // parameter to false.
+        mBluetoothGatt = device.connectGatt(this, false, mGattCallback);
+        Log.d(TAG, "Trying to create a new connection.");
+        mBluetoothDeviceAddress = address;
+        mConnectionState = STATE_CONNECTING;
+        return true;
+    }
