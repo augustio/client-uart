aaebeb000095201fb2e42568aaac7a3ad6465eb0
Augustine Onubeze <augustine.onubeze@metropolia.fi>
2015-04-14 12:06:05 +0300

    Add close member function to UartService from nordicsemi nrfUARTv2

--- ElectriaHRM/app/src/main/java/electria/electriahrm/UartService.java
+++ ElectriaHRM/app/src/main/java/electria/electriahrm/UartService.java
@@ -275,5 +275,18 @@ public class UartService extends Service {
         mBluetoothGatt.disconnect();
     }
 
+    /**
+     * After using a given BLE device, the app must call this method to ensure resources are
+     * released properly.
+     */
+    public void close() {
+        if (mBluetoothGatt == null) {
+            return;
+        }
+        Log.w(TAG, "mBluetoothGatt closed");
+        mBluetoothDeviceAddress = null;
+        mBluetoothGatt.close();
+        mBluetoothGatt = null;
+    }
 
 }
