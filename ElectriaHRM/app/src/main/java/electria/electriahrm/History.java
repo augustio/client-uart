328340fa20f2a4d8b1a7d554f47827c38b0bdcce
Augustine Onubeze <augustine.onubeze@metropolia.fi>
2015-05-17 12:46:16 +0300

    Declare and initialize history list components

--- ElectriaHRM/app/src/main/java/electria/electriahrm/History.java
+++ ElectriaHRM/app/src/main/java/electria/electriahrm/History.java
@@ -2,14 +2,23 @@ package electria.electriahrm;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class History extends Activity {
+
+    private ListView historyView;
+    private ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        historyView = (ListView) findViewById(R.id.historyListView);
+        listAdapter = new ArrayAdapter<String>(this, R.layout.message_detail);
+        historyView.setAdapter(listAdapter);
+        historyView.setDivider(null);
     }
 }
