8c0d4b86446287b2cd0c016602bbcb24c3c021dc
Augustine Onubeze <augustine.onubeze@metropolia.fi>
2015-05-18 11:21:38 +0300

    Remove unnecessary imports

--- ElectriaHRM/app/src/main/java/electria/electriahrm/HistoryDetail.java
+++ ElectriaHRM/app/src/main/java/electria/electriahrm/HistoryDetail.java
@@ -12,9 +12,7 @@ import org.achartengine.GraphicalView;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 
 
 public class HistoryDetail extends Activity {
