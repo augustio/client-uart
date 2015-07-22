d90f6b23a87a9141a08102a67585ec577184f591
Augustine Onubeze <augustine.onubeze@metropolia.fi>
2015-07-22 13:11:26 +0300

    Modify ECGMeasurement class

--- ElectriaHRM/app/src/main/java/electria/electriahrm/ECGMeasurement.java
+++ ElectriaHRM/app/src/main/java/electria/electriahrm/ECGMeasurement.java
@@ -1,36 +1,59 @@
 package electria.electriahrm;
 
-/**
- * Created by augustio on 15.7.2015.
- */
+import com.google.gson.Gson;
+
+
 public class ECGMeasurement {
 
     private String id;
     private String timeStamp;
     private String data;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
+    public void ECGMeasurement(String id, String timeStamp){
         this.id = id;
-    }
-
-    public String getTimeStamp() {
-        return timeStamp;
-    }
-
-    public void setTimeStamp(String timeStamp) {
         this.timeStamp = timeStamp;
     }
 
-    public String getData() {
+    protected String getId() {
+        return id;
+    }
+
+    protected void setId(String id) {
+        this.id = id;
+    }
+
+    protected String getTimeStamp() {
+        return timeStamp;
+    }
+
+    protected void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    protected String getData() {
         return data;
     }
 
-    public void setData(String data) {
+    protected void setData(String data) {
         this.data = data;
+    }
+
+    protected void addValue(String value){
+        data = value + "\n";
+    }
+
+    protected String toJson(){
+
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+
+    protected void fromJson(String json){
+        Gson gson = new Gson();
+        ECGMeasurement ecgM = gson.fromJson(json, ECGMeasurement.class);
+        this.id = ecgM.getId();
+        this.timeStamp = ecgM.getTimeStamp();
+        this.data = ecgM.getData();
     }
 
 }
